f8197578b5a6aeb72a3862496d89e8936cac0dce
goncalveshigino <goncalveshigino20@gmail.com>
2023-08-08 12:01:06 +0100

    fit: KeyValue Pipe

--- src/app/products/pages/un-common-page/un-common-page.component.ts
+++ src/app/products/pages/un-common-page/un-common-page.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class UnCommonPageComponent {
 
+  //i18n Select
+  public name: string = 'Higino';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  changeClient():void {
+    this.name = 'Lidia',
+    this.gender = 'female'
+  }
+
+  //i18nPlural
+  public clientes: string[] = ['Higino','Katia','Lidia','Neidy','Gloria','Deolinga','Katiavala','Douglas'];
+  public clientsMap = {
+  '=0': 'nao temos nenhum cliente esperando',
+  '=1': 'temos um cliente esperando',
+  '=2': 'temos 2 pessoas esperando',
+  'other': 'temos # clientes esperando.'
+  }
+
+  deleteClient():void {
+    this.clientes.shift();
+  }
+
+
+  //KeyValue Pipe
+  public person = {
+    name: 'Higino',
+    age: 26,
+    address: 'Angola-Menongue'
+  }
+
 }
